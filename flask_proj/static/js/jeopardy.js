99d977caa9da4653d836f6a469089de10742d410
Amber Tin <amti9774@colorado.edu>
2024-04-29 08:05:10 +0000

    Use innerText instead of TextContent

--- flask_proj/static/js/jeopardy.js
+++ flask_proj/static/js/jeopardy.js
@@ -70,7 +70,7 @@ function fetchCategories() {
 		.then(data => {
 			// Update the category headers on the gameplay page
 			for (let i = 0; i < data.length; i++) {
-				document.getElementById(`categ_${i + 1}`).textContent = data[i].CategName;
+				document.getElementById(`categ_${i + 1}`).innerText = data[i].CategName;
 			}
 		})
 		.catch(error => console.error('Error fetching categories:', error));
