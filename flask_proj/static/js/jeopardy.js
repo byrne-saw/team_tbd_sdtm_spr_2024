c313f5a24316622e4d90d599db4d8c9e78a9a796
Amber Tin <amti9774@colorado.edu>
2024-04-29 09:03:26 +0000

    js

--- flask_proj/static/js/jeopardy.js
+++ flask_proj/static/js/jeopardy.js
@@ -61,43 +61,8 @@ function submitPlayerName() {
 document.getElementById('submit-name-button').addEventListener('click', submitPlayerName);
 
 
-// ************** Categories  ***************
-
-// function to fetch categories from Flask route and populate the gameplay page
-function fetchCategories() {
-	fetch('/categories')
-		.then(response => response.json())
-		.then(data => {
-			// Update the category headers on the gameplay page
-			for (let i = 0; i < data.length; i++) {
-				document.getElementById(`categ_${i + 1}`).innerText = data[i].CategName;
-			}
-		})
-		.catch(error => console.error('Error fetching categories:', error));
-}
-
-// call fetchCategories() when the window is fully loaded
-window.onload = fetchCategories;
 
 
-// Function to handle "Random" category button click
-document.getElementById('random-button').addEventListener('click', () => {
-	// Send an AJAX request to fetch random categories from the server
-	// Display the fetched categories in the 'categories' div
-	fetch('/random-categories')
-		.then(response => response.json())
-		.then(data => {
-			const categoriesDiv = document.getElementById('categories');
-			categoriesDiv.innerHTML = ''; // Clear previous categories
 
-			// Display the fetched categories
-			data.categories.forEach(category => {
-				const categoryElement = document.createElement('div');
-				categoryElement.textContent = category;
-				categoriesDiv.appendChild(categoryElement);
-			});
-		})
-		.catch(error => console.error('Error fetching random categories:', error));
-});
 
 
